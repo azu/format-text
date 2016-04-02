6769851467de2983eb569a622c01b9538d308955
azu <azuciao@gmail.com>
2016-04-02 17:49:40 +0900

    fix(test): use mocha instead of prova

--- test.js
+++ test.js
@@ -1,25 +1,30 @@
+var assert = require("assert");
 var format = require("./");
 
-it('substitutes {\d} with remaning args', function(){
+it('substitutes {\d} with remaning args', function () {
 
-  expect(format('foo {0} bar {1} qux {2}', 'corge', 'span', 'quux')).to.equal('foo corge bar span qux quux');
-
-});
-
-it('substitutes {\w} with given context object', function(){
-
-  expect(format('foo {corge} bar {qux}', { corge: 3, qux: 14 })).to.equal('foo 3 bar 14');
+    assert.equal(format('foo {0} bar {1} qux {2}', 'corge', 'span', 'quux'), 'foo corge bar span qux quux');
 
 });
 
-it('calls functions passed within context', function(){
+it('substitutes {\w} with given context object', function () {
 
-  expect(format('foo {corge} bar {qux}', { corge: function(){ return 3 }, qux: 14 })).to.equal('foo 3 bar 14');
+    assert.equal(format('foo {corge} bar {qux}', {corge: 3, qux: 14}), 'foo 3 bar 14');
 
 });
 
-it('can be escaped by doubling: {{ and }}', function(){
+it('calls functions passed within context', function () {
 
-  expect(format('foo {0}, bar {{1}}', 3, 14)).to.equal('foo 3, bar {1}');
+    assert.equal(format('foo {corge} bar {qux}', {
+        corge: function () {
+            return 3
+        }, qux: 14
+    }), 'foo 3 bar 14');
+
+});
+
+it('can be escaped by doubling: {{ and }}', function () {
+
+    assert.equal(format('foo {0}, bar {{1}}', 3, 14), 'foo 3, bar {1}');
 
 });
